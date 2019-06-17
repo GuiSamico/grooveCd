1d105bb492059d6e9ff20e27f06936ea17cb311b
Guilherme Silva <guilhermesamico11@gmail.com>
2019-06-17 00:14:17 -0300

    v1.1

---
+++ index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Groove CD</title>
+    <link rel="shortcut icon" href="img/favicon.png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:400">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div id="main_menu">
+        <div class="logo_area">
+            <a href="#"><img src="img/logo.png"></a>
+        </div>
+        <div class="inner_main_menu">
+            <ul>
+                <li><a href="admin.php">Administrador</a></li>
+                <li><a href="">Login</a></li>
+                <li><a href="cadastro-cli">Cadastro</a></li>
+                <li><a href="" onclick="return alert('Faça login para mais recursos!')">Música</a></li>
+                <li><a href="" onclick="return alert('Faça login para mais recursos!')">Artista</a></li>
+                <li><a href="" onclick="return alert('Faça login para mais recursos!')">Gênero</a></li>
+            </ul>
+        </div>
+    </div>
+    <h1 style="margin-top:140px; margin-left:50px; margin-bottom:-30px;">Principais faixas de cada gênero</h1>
+    <div class="container">
+        <div class="box">
+            <div class="imgBox">
+                <img src="img/alineBarros.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                    <h2>Aline Barros</h2>
+                    <p>Ressucita-me</p>
+                    <audio controls style="width:250px; position:relative; top:50px;">
+                        <source src="audio/ressuscita-me.mp3" type="audio/mpeg">
+                    </audio>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="imgBox">
+                <img src="img/amy.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                    <h2>Amy Winehouse</h2>
+                    <p>Back to black</p>
+                    <audio controls style="width:250px; position:relative; top:50px;">
+                        <source src="audio/back-to-black.mp3" type="audio/mpeg">
+                    </audio>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="imgBox">
+                <img src="img/beethoven.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                    <h2>Beethoven</h2>
+                    <p>Fur elise</p>
+                    <audio controls style="width:250px; position:relative; top:50px;">
+                        <source src="audio/fur-elise.mp3" type="audio/mpeg">
+                    </audio>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="imgBox">
+                <img src="img/belo.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                    <h2>Belo</h2>
+                    <p>Tudo mudou</p>
+                    <audio controls style="width:250px; position:relative; top:50px;">
+                        <source src="audio/tudo-mudou.mp3" type="audio/mpeg">
+                    </audio>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="imgBox">
+                <img src="img/bobMarley.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                    <h2>Bob Marley</h2>
+                    <p>Is this love</p>
+                    <audio controls style="width:250px; position:relative; top:50px;">
+                        <source src="audio/is-this-love.mp3" type="audio/mpeg">
+                    </audio>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="imgBox">
+                <img src="img/bruno.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                    <h2>Bruno Mars</h2>
+                    <p>Just the way you are</p>
+                    <audio controls style="width:250px; position:relative; top:50px;">
+                        <source src="audio/just-the-way-you-are.mp3" type="audio/mpeg">
+                    </audio>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="imgBox">
+                <img src="img/edSheeran.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                    <h2>Ed Sheeran</h2>
+                    <p>Perfect</p>
+                    <audio controls style="width:250px; position:relative; top:50px;">
+                        <source src="audio/perfect.mp3" type="audio/mpeg">
+                    </audio>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="imgBox">
+                <img src="img/eminem.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                    <h2>Eminem</h2>
+                    <p>Rap god</p>
+                    <audio controls style="width:250px; position:relative; top:50px;">
+                        <source src="audio/rap-god.mp3" type="audio/mpeg">
+                    </audio>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="imgBox">
+                <img src="img/mariliaMendonca.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                    <h2>Marilia Mendoça</h2>
+                    <p>Ausência</p>
+                    <audio controls style="width:250px; position:relative; top:50px;">
+                        <source src="audio/ausencia.mp3" type="audio/mpeg">
+                    </audio>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="imgBox">
+                <img src="img/marshmallo.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                    <h2>Marshmallo</h2>
+                    <p>Alone</p>
+                    <audio controls style="width:250px; position:relative; top:50px;">
+                        <source src="audio/alone.mp3" type="audio/mpeg">
+                    </audio>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="imgBox">
+                <img src="img/queen.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                    <h2>Queen</h2>
+                    <p>Bohemian rhapsody</p>
+                    <audio controls style="width:250px; position:relative; top:50px;">
+                        <source src="audio/bohemian-rhapsody.mp3" type="audio/mpeg">
+                    </audio>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+            <div class="imgBox">
+                <img src="img/xand.jpg" alt="">
+            </div>
+            <div class="details">
+                <div class="content">
+                    <h2>Xand Avião</h2>
+                    <p>Sábado à noite</p>
+                    <audio controls style="width:250px; position:relative; top:50px;">
+                        <source src="audio/sabado-a-noite.mp3" type="audio/mpeg">
+                    </audio>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
